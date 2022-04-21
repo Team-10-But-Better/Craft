246e95a54bb64cd04599649df2b78ff0322a0d2b
isdp2001 <isaacpartee@outlook.com>
2022-04-21 11:32:23 -0400

    added tests for crouching

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -38,6 +38,30 @@ TEST(tests, issue9)
 	EXPECT_EQ(getGroundedMovementSpeed(true, &teleportHeld, 5), 5);
 }
 
+extern "C" int getCrouchingHeight(bool crouch, bool *crouchingHeldLastFrame);
+extern "C" bool isCrouching(bool crouch);
+
+/// [issue](https://github.com/Team-10-But-Better/Craft/issues/69)
+TEST(test, issue69)
+{
+
+	bool crouchingHeldLastFrame = false;
+	EXPECT_EQ(getCrouchingHeight(false, &crouchingHeldLastFrame), 2);
+	EXPECT_EQ(crouchingHeldLastFrame, false);
+	crouchingHeldLastFrame = false;
+	EXPECT_EQ(getCrouchingHeight(true, &crouchingHeldLastFrame), 1);
+	EXPECT_EQ(crouchingHeldLastFrame, true);
+	crouchingHeldLastFrame = true;
+	EXPECT_EQ(getCrouchingHeight(false, &crouchingHeldLastFrame), 2);
+	EXPECT_EQ(crouchingHeldLastFrame, false);
+	crouchingHeldLastFrame = true;
+	EXPECT_EQ(getCrouchingHeight(true, &crouchingHeldLastFrame), 1);
+	EXPECT_EQ(crouchingHeldLastFrame, true);
+
+	EXPECT_EQ(isCrouching(true), true);
+	EXPECT_EQ(isCrouching(false), false);
+}
+
 //----------------------------------------------------
 int main(int argc, char* argv[])
 {
